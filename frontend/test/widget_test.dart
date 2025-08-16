336f4e3ecb6d3c8dbfc66ff4e6603f0de2518ccb
홍철수 <hongcs1018@gmail.com>
2025-08-16 15:39:21 +0900

    test(frontend): rename package import to k_medical

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:healing_on/main.dart';
+import 'package:k_medical/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
